679de90237b490944613b57020b396f5d0401a61
yudiamond <adamyuda60@gmail.com>
2026-05-18 05:21:20 -0700

    Create DATA.lua

---
+++ DATA.lua
@@ -0,0 +1,12 @@
+return {
+    "PlayerSatu",
+    "PlayerDua",
+    "PlayerTiga",
+    "PlayerEmpat",
+    "PlayerLima",
+    "PlayerEnam",
+    "PlayerTujuh",
+    "PlayerDelapan",
+    "PlayerSembilan",
+    "PlayerSepuluh",
+}
